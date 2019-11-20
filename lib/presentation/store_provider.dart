056bc61fa5f936af8cd5957793680595830adcd0
Renesanse <dsrenesanse@gmail.com>
2019-11-20 11:41:39 +0300

    added lifecycle wrapper

--- lib/presentation/store_provider.dart
+++ lib/presentation/store_provider.dart
@@ -19,3 +19,35 @@ class StoreProvider<S extends Store<BaseState>> extends StatelessWidget {
 
   static S of<S extends Store<BaseState>>(BuildContext context) => Provider.of<S>(context);
 }
+
+// ignore: must_be_immutable
+class _LifecycleWrapper<S extends Store<BaseState>> extends StatefulWidget {
+  Widget child;
+  @override
+  _LifecycleWrapperState createState() => _LifecycleWrapperState<S>();
+}
+
+class _LifecycleWrapperState<S extends Store<BaseState>> extends State<_LifecycleWrapper>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    StoreProvider.of<S>(context).storeState();
+  }
+}
