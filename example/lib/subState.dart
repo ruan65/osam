d8572de15a67ce891e21c4d12ae565b108acb22e
Renesanse <dsrenesanse@gmail.com>
2019-11-27 21:26:03 +0300

    updated dependencies

--- example/lib/subState.dart
+++ example/lib/subState.dart
@@ -4,8 +4,8 @@ import 'package:osam/domain/state/base_state.dart';
 
 part 'subState.g.dart';
 
-// ignore: must_be_immutable
 @HiveType()
+// ignore: must_be_immutable
 class SubState extends BaseState<SubState> {
   @HiveField(0)
   int count = 0;
--- test/test.dart
+++ test/test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('fits', () {
-    final map = {1: '1', 2: '2'};
-    map.remove(1);
-  });
+  test('fits', () {});
 }
